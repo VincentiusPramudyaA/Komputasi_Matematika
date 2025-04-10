cb5d969525eab044c73188b75fb47417bbd213d6
VincentiusPramudyaA <vinentiuspramudyaa@gmail.com>
2025-04-10 11:18:46 +0700

    first init github

---
+++ PRAKTIKUM_2.m
@@ -0,0 +1,18 @@
+clc
+clear
+
+%menghitung luas persegi panjang
+p = 10;
+l = 20;
+lp = p*l;
+disp(lp)
+
+%operasi matematika 
+lp1 = p+1;
+lp2 = p-1;
+lp3 = p\1;
+
+disp(lp1)
+disp(lp2)
+disp(lp3)
+
