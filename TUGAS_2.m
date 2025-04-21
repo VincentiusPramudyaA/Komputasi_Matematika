050882ad9290ac6d456919bdbc066361d9d66186
VincentiusPramudyaA <vinentiuspramudyaa@gmail.com>
2025-04-21 07:55:15 +0700

    tugas 2

---
+++ TUGAS_2.m
@@ -0,0 +1,20 @@
+%nested if
+%umur = input('umur: ');
+%if umur < 17
+%   pendamping = input('apakah ada pendamping orqng tua?(y/n): ', 's');
+  
+%   if pendamping == 'y'
+%      disp('boleh masuk bioskop');
+%   else
+%      disp('tidak boleh masuk bioskop');
+%   end
+%else
+%   disp('boleh masuk bioskop tanpa pendamping');
+%end
+
+%while loop
+%angka = 0; 
+%while angka ~= 10  
+%    angka = input('Masukkan angka (ketik 10 untuk keluar): ');  
+%end
+%disp('Terima kasih!'); 
